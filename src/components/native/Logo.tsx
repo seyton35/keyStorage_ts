090a0c0b7e2c6433569257ac54890ddfbf8425b9
seyton35 <prosto99-00@mail.ru>
2023-04-19 00:05:03 +0300

    custom Logo component

---
+++ src/components/native/Logo.tsx
@@ -0,0 +1,41 @@
+import { Image, ImageStyle, ViewStyle, StyleSheet, View } from 'react-native'
+
+import { accountsLogo_48x48 as logos } from '../../../assets/accountsLogo_48x48'
+
+interface Params {
+    title: string,
+    size?: number,
+    style?: ViewStyle,
+    iconStyle?: ImageStyle,
+}
+
+export default function Logo({ title, size = 30, style, iconStyle }: Params) {
+    if (!logos[title]) {
+        title = 'key'
+    }
+
+    return (
+        <View style={[
+            styles.logoBox,
+            style
+        ]}>
+            <Image style={[
+                styles.logo,
+                { width: size, height: size },
+                iconStyle
+            ]}
+                source={logos[title]}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    logoBox: {
+
+    },
+    logo: {
+        borderRadius: 10,
+        backgroundColor: '#fff'
+    }
+})
