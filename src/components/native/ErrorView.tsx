3a3bed2de0efed79b31f775509ec83137d9cad9b
seyton35 <prosto99-00@mail.ru>
2023-04-26 23:52:40 +0300

    animated ErrorViev

--- src/components/native/ErrorView.tsx
+++ src/components/native/ErrorView.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Animated } from 'react-native'
 import Txt from '../custom/Txt'
+import { useEffect, useRef } from 'react'
 
 interface Params {
     text: string | null
@@ -7,17 +8,51 @@ interface Params {
 }
 
 export default function ErrorView({ text, style }: Params) {
+    const height = useRef(new Animated.Value(0)).current
+    useEffect(() => {
+        if (text !== null)
+            increace()
+    }, [text])
+
+    const interpolate = height.interpolate(
+        { inputRange: [0, 1], outputRange: [0, 30] }
+    )
+
+    function increace() {
+        Animated.timing(
+            height,
+            {
+                toValue: 1,
+                useNativeDriver: false,
+            }
+        ).start(() => {
+            setTimeout(() => {
+                deCreace()
+            }, 3000);
+        })
+    }
+
+    function deCreace() {
+        Animated.timing(
+            height,
+            {
+                toValue: 0,
+                useNativeDriver: false,
+            }
+        ).start()
+    }
+
     return (
-        <View style={[styles.errorView, style]}>
+        <Animated.View style={[styles.errorView, {
+            height: interpolate
+        }, style]}>
             {text && <Txt style={styles.errorTxt}>{text}</Txt>}
-        </View>
+        </Animated.View>
     )
 }
 
 const styles = StyleSheet.create({
-    errorView: {
-        height: 25
-    },
+    errorView: {},
     errorTxt: {
         color: 'red',
         fontSize: 15.5
